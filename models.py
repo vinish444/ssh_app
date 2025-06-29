d4f7ff6eab96d3c2b72c90c1e9460346ff470003
vinish444 <103312902+vinish444@users.noreply.github.com>
2025-06-29 21:23:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
-from gui_apps.ss.db import Base
+from gui_apps.ssh_app.db import Base
 from datetime import datetime
 
 class TerminalServer(Base):
